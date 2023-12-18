60071b08a1895cec331f2b72839ee7fd5737125b
Kazunobu Kawane <k.kawane.1231@gmail.com>
2023-12-18 18:19:31 +0900

    feat: modular-formsで書き換え

--- src/RegisterUserForm.tsx
+++ src/RegisterUserForm.tsx
@@ -1,19 +1,10 @@
-import { valibotResolver } from "@hookform/resolvers/valibot";
-import { useForm, SubmitHandler } from "react-hook-form";
 import * as stylex from "@stylexjs/stylex";
-
+import { useForm, SubmitHandler, valiForm } from "@modular-forms/react";
 import { registerUserSchema, RegisterUser } from "./schema";
 
 export function RegisterUserForm() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<RegisterUser>({
-    defaultValues: {
-      shouldStayLoggedIn: false,
-    },
-    resolver: valibotResolver(registerUserSchema),
+  const [registerUserForm, { Form, Field }] = useForm<RegisterUser>({
+    validate: valiForm(registerUserSchema),
   });
 
   const onSubmit: SubmitHandler<RegisterUser> = (data) => {
@@ -21,41 +12,53 @@ export function RegisterUserForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <Form onSubmit={onSubmit}>
       <div {...stylex.props(styles.textboxRow)}>
         <label htmlFor="email">email</label>
         <div {...stylex.props(styles.textboxContainer)}>
-          <input
-            id="email"
-            type="email"
-            {...register("email")}
-            {...stylex.props(styles.textbox)}
-          />
-          {errors.email && (
-            <p {...stylex.props(styles.errorMsg)}>{errors.email.message}</p>
-          )}
+          <Field name="email">
+            {(field, props) => (
+              <>
+                <input
+                  {...props}
+                  id="email"
+                  type="email"
+                  {...stylex.props(styles.textbox)}
+                />
+                {field.error && (
+                  <p {...stylex.props(styles.errorMsg)}>{field.error}</p>
+                )}
+              </>
+            )}
+          </Field>
         </div>
       </div>
       <div {...stylex.props(styles.textboxRow)}>
         <label htmlFor="password">password</label>
         <div {...stylex.props(styles.textboxContainer)}>
-          <input
-            id="password"
-            type="password"
-            {...register("password")}
-            {...stylex.props(styles.textbox)}
-          />
-          {errors.password && (
-            <p {...stylex.props(styles.errorMsg)}>{errors.password.message}</p>
-          )}
+          <Field name="password">
+            {(field, props) => (
+              <>
+                <input
+                  {...props}
+                  id="password"
+                  type="password"
+                  {...stylex.props(styles.textbox)}
+                />
+                {field.error && (
+                  <p {...stylex.props(styles.errorMsg)}>{field.error}</p>
+                )}
+              </>
+            )}
+          </Field>
         </div>
       </div>
       <div {...stylex.props(styles.checkboxRow)}>
-        <input
-          id="should-stay-logged-in"
-          type="checkbox"
-          {...register("shouldStayLoggedIn")}
-        />
+        <Field name="shouldStayLoggedIn" type="boolean">
+          {(_, props) => (
+            <input {...props} id="should-stay-logged-in" type="checkbox" />
+          )}
+        </Field>
         <label htmlFor="should-stay-logged-in">
           次回から自動的にログインする
         </label>
@@ -63,7 +66,7 @@ export function RegisterUserForm() {
       <button type="submit" {...stylex.props(styles.button)}>
         会員登録する
       </button>
-    </form>
+    </Form>
   );
 }
 
